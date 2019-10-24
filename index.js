21581d297daf1a5e874aa9289894b5a3661d28ec
limjihun <ljh890525@gmail.com>
2019-10-24 11:13:54 +0900

    android release error 해결 [ react-native-gesture-hadler ]

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 /**
  * @format
  */
+/**
+ * android release apk 에러
+ * Invariant Violation: Unsupported top level event type "onGestureHandlerStateChange" dispatched
+ * react-native 0.61.2 & react-navigation / react-native-gesture-hadler 에러
+ * 해결 import 'react-native-gesture-handler'
+ */
+import 'react-native-gesture-handler'
 
 import { AppRegistry } from 'react-native';
 import App from './src';
